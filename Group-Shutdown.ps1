3272af4afda13c64fa928aa8a3fdf8c4936cbf8f
CTBolland <48163154+CTBolland@users.noreply.github.com>
2019-03-19 17:11:53 -0700

    Update Group-Shutdown.ps1

--- Group-Shutdown.ps1
+++ Group-Shutdown.ps1
@@ -4,40 +4,40 @@
     Group Shutdown of Remote Computers
     
 .DESCRIPTION
-    Pulls from a CSV list and shutsdown servers. If its a vm, it sends a "Stop-VM $name" command to the host.
-    This way the script need not connect to every single VM, but simply connect to their host. This is
-    done for security reasons, and ease of management. If it's a physical box, it sends a "shutdown -s -m \\$name". 
-    This command requires that the physical server is configured to allow remote shutdown. This script is best 
-    used in conjunction with task scheduler, or paired with a power management solution such as Eaton.   
+    Pulls from a CSV list and shuts down servers. If Server is a Hyper-V VM, it sends a "Stop-VM $name" command to the host.
+    This way the script need not connect to every single VM, but simply connect to their host. 
+    It first checks if the VM is located on a Cluster, which are defiend at the top of the script. 
+    This is done for security reasons, and ease of management. 
+    If it's a physical box, it sends a "shutdown -s -m \\$name". Note: This command requires that the physical server 
+    is configured to allow remote shutdown. 
+    
+    This script is best used in conjunction with task scheduler, or paired with a power management solution such as Eaton.   
     
 .INPUTS
     This CSV that contains all the device information should have the following columns:
         name --- Device name
         ip ----- IP address of device
         type --- Host or Guest
-        host --- If type is guest, then host name goes here
+        host --- If type is guest, then host/cluster name goes here
         group -- Assign device to a group. These groups are used to target collections of devices
 
     Example CSV:
 
     name        ip              type    host        group
     ---------------------------------------------------
-    Server01    192.168.0.8     host                B
-    vm01        192.168.0.9     guest   Server01    A   
-    vm02        192.168.0.19    guest   Server01    A
+    Server01    192.168.0.10     host                B
+    vm01        192.168.0.20     guest   Server01    A   
+    vm02        192.168.0.30     guest   Server01    A
     
 .PARAMETER group
     Target specifc groups within the csv  
-
-.PARAMETER list
-    Path to the CSV device list
-
+    
 .EXAMPLE
     Shutdown group A, which is comprised of the guests running on Server01:
-        .\Group-Shutdown.ps1 -list "C:\Temp\servers.csv" -group "A"
+        .\Group-Shutdown.ps1 -group "A"
 
     Shutdown group B, which is the host:
-        .\Group-Shutdown.ps1 -list "C:\Temp\servers.csv" -group "B"
+        .\Group-Shutdown.ps1 -group "B"
 
 .NOTES       
     AUTHOR:         Craig Bolland
@@ -45,44 +45,53 @@
     UPDATED:        03-19-2019 
 #> 
 # ---------------------------------------------------------- 
-param (
-    [string]$list,
-    [string]$group
-    )  
+param ([string]$group)  
+
+$list = "c:\Temp\Servers.csv"
+$logPath = "c:\Temp\logs"
+$clusters = @("clusterA", "clusterB") # <--add or $null failover clusters here
+
 # logging variables and function
-$log = "C:\Scripts\Eaton\Server\Logs\" + (Get-Date).tostring("MM-dd-yyyy") + "-Server Shutdown.log" 
+$log = "$logPath\Server-Shutdown-Log-" + (Get-Date).tostring("MM-dd-yyyy") + ".txt" 
 if(!(Test-Path -Path $log )){New-Item -ItemType File -Path $log}
 function Write-Log {
-    param (
-        [string]$Message
-    )  
-    $time = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
-    Write-Output "$($time)Group[$group]-[$ip]-[$name]-[$hoster] $message"| Out-file $log  -Append -Force
+    param ([string]$Message)  
+    $time = "[{0:HH:mm:ss}]" -f (Get-Date)
+    Write-Output "$($time)Group[$group]-[$ip]-[$hostName]-[$name] $message"| Out-file $log  -Append -Force
 }
+
 # select all devices in the specified group
-$servers = Import-Csv -Path $list | Where-Object {$_.Group -eq $group} 
+$servers = Import-Csv -Path $list| Where-Object {$_.Group -eq $group} 
 foreach ($server in $servers) {
-    # csv variables
     # csv variables
     $ip = $server.ip
     $name = $server.name
     $hostName = $server.host
     $type = $server.type
-    # determine if VM
+
+# If the device is a guest VM on a cluster or a standalone host:
     if ($type -eq "Guest") { 
-        Stop-VM –Name $name –ComputerName (Get-ClusterNode –Cluster $hostName) -WarningVariable a
-        Write-Log $a
-        Remove-PSSession $hoster
-    # determine if physical box
+        # if hosted on a cluster:
+        if ($hostName -in $clusters) {
+            Stop-VM –Name $name –ComputerName (Get-ClusterNode –Cluster $hostName) -WarningVariable a
+            if ($a -eq $null) { Write-Log -Message "SUCESS, intiating shutdown" } else { Write-Log $a }
+        # if hosted on a standalone host:
+        } else {
+            Stop-VM –Name $name –ComputerName ($hostName) -WarningVariable a
+            if ($a -eq $null) { Write-Log -Message "SUCESS, intiating shutdown" } else { Write-Log $a }
+        }
+
+# If the device is a physical server:
     } elseif ($type -eq "Host" -or "Physical") {
-        shutdown -s -m \\$name -t 1 /f 
-        # the sever will either shutdown or return an error. If an error, it's either
-        # because its offline or not configured for remote shutdown. This determines which.
+        shutdown -s -m \\$name -t 1 /f /d p:0:0 /c "Admin Initiated Shutdown"
+        # if throws error and is pingable:
         if ($LastExitCode -ne 0 -And (Test-Connection -computername $ip -Quiet -Count 1)) {
             Write-Log -Message "ERROR($LastExitCode)"
+        # if throws error and is not pingable:
         } elseif ($LastExitCode -ne 0 -And (!(Test-Connection -computername $ip -Quiet -Count 1))) {
             Write-Log -Message "OFFLINE"
-        } else {
+        # no error:
+        } else { 
             Write-Log -Message "SUCESS, intiating shutdown"
         }
     }
