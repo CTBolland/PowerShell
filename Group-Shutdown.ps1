27f7e8f6011e94f0dc6922c6d97e72946fc8ebcc
CTBolland <48163154+CTBolland@users.noreply.github.com>
2019-03-22 09:45:53 -0700

    Update Group-Shutdown.ps1

--- Group-Shutdown.ps1
+++ Group-Shutdown.ps1
@@ -1,7 +1,7 @@
 #REQUIRES -Version 2.0
 <# 
 .SYNOPSIS
-    Group Shutdown of Virtual and Physical Servers, via the Hosts.
+    Group Shutdown of Virtual and Physical Servers.
     
 .DESCRIPTION
     Pulls from a CSV list and shuts down servers. If Server is a Hyper-V VM, it sends a "Stop-VM $name" command 
@@ -49,27 +49,27 @@
     UPDATED:        03-19-2019 
 #> 
 # ---------------------------------------------------------- 
-param (
-    [string]$group
-)  
+param ([string]$group)  
 
+$group = "a"
 $deviceList = "c:\temp\servers.csv"
 $logFolder = "c:\temp\logs"
-$log = ("$logFolder\Server-Shutdown-Log-" + (Get-Date).tostring("MM-dd-yyyy") + ".txt")
 $clusters = @("cluster1", "cluster2")
 $pTypes = @("host", "physical", "cluster*")
 $guest = "guest"
+$log = ("$logFolder\Server-Shutdown-Log-" + (Get-Date).tostring("MM-dd-yyyy") + ".csv")
 
-$log = ("$logFolder\Server-Shutdown-Log-" + (Get-Date).tostring("MM-dd-yyyy") + ".txt")
 if(!(Test-Path -Path $log )){New-Item -ItemType File -Path $log}
 
 function Write-Log {
-    param (      
-        [string]$status,
-        [string]$Message
-    )    
+    param ([string]$code)
+
+    if     ( $code -eq 1 ) { $status = 'SHUTTING OFF'; $message = "The virtual machine is shutting down"  }
+    elseif ( $code -eq 2 ) { $status = 'OFF'; $message = "The virtual machine is already in the specified state." }
+    elseif ( $code -eq 3 ) { $status = 'ERROR'; $message = "Device not reachable" }
+    
     $time = "{0:HH:mm:ss}" -f (Get-Date)
-    $line = [pscustomobject]@{
+    [pscustomobject]@{
         'DateTime' = $time
         'Status' = $status
         'Group' = $group
@@ -77,12 +77,12 @@ function Write-Log {
         'Type' = $type
         'Host' = $hoster
         'IP' = $ip
-        'Message' = $Message    
-    }   
-    $line | Export-Csv -Path $Log -Append -NoTypeInformation
-}
+        'Message' = $message
+    } | Export-Csv -Path $log  -force -Append -NoTypeInformation
+}    
 
-# select all devices in the specified group
+# --------------------------------------------------------------
+
 $servers = Import-Csv -Path $deviceList| Where-Object {$_.Group -eq $group} 
 foreach ($server in $servers) {
     # csv variables
@@ -90,37 +90,49 @@ foreach ($server in $servers) {
     $name = $server.name
     $type = $server.type
     $hoster = $server.host
-    
-# determine if VM is on cluster or standalone server host:
-    # if hosted in a failover cluster:
-    if (($type -eq $guest) -and ($hoster -in $clusters)) {
-        # find and shut down vm on whatever node it is currently on
-        Get-ClusterNode –Cluster $hoster | Get-ClusterResource -Name *$name | Get-VM | Stop-VM -Force -WarningVariable a
-        if ($a -eq $null) { Write-Log -status "SHUTDOWN" -Message "Shutdown Initiated" } else { Write-Log -status "OFFLINE" -message $a }
-    } 
-    # if hosted on a standalone host:
-    elseif (($type -eq $guest) -and ($hoster -notin $clusters)) {
-        Stop-VM –Name $name –ComputerName ($hoster) -Force -WarningVariable a
-        if ($a -eq $null) { Write-Log -status "SHUTDOWN" -Message "Shutdown Initiated"  } else { Write-Log -status "OFFLINE" -message $a }
-    }
 
-# If the device is a physical server:
+# --------------------------------------------------------------
+# if hosted within failover cluster:
+    if (($type -eq $guest) -and ($hoster -in $clusters)) {
+       $vmNode = Get-ClusterNode –Cluster $hoster | Get-ClusterResource -Name *$name 
+        $vmNode | Select-Object -Index 0 | Get-VM | Stop-VM -Force -WarningVariable a
+        if ($a -eq $null) { 
+            Write-Log -code 1 
+        } 
+        else { 
+            Write-Log -code 2 
+        }
+    }
+# --------------------------------------------------------------
+# if hosted on standalone host:
+    elseif (($type -eq $guest) -and ($hoster -notin $clusters)) {
+        if ((get-vm -name $name -ComputerName $hoster).state -eq 'Running') {
+            Stop-VM –Name $name –ComputerName $hoster -Force; 
+            Write-Log -code 1 
+        }
+        elseif ((get-vm -name $name -ComputerName $hoster).state -eq 'Off') { 
+            Write-Log -code 2 
+        }
+        else { 
+            Write-Log -code 3 
+        }
+    }
+# --------------------------------------------------------------
+#   If device is a physical server:
     elseif ($type -in $pTypes) {
         # shutdown command
-        shutdown -s -m \\$ip -t 1 /f
+        shutdown -s -m \\$ip -t 1 /f 
         # error and is pingable:
-        if ($LastExitCode -ne 0 -And (Test-Connection -computername $ip -Quiet -Count 1)) {
-            Write-Log -status "MISCONFIGURED" -Message "Error Code($LastExitCode)"
+        if ($LastExitCode -ne 0 -And (Test-Connection -computername $ip -Quiet -Count 1)) { 
+            Write-Log -code 3
         }
         # error and is not pingable:
         elseif ($LastExitCode -ne 0 -And (!(Test-Connection -computername $ip -Quiet -Count 1))) {
-            Write-Log -status "OFFLINE" -message "Computer already powered off"
+            Write-Log -code 2
         }
         # no error:
         else { 
-            Write-Log -status "ACCEPTED" -message "Shutdown Initiated"
+            Write-Log -code 1
         }
     }
 }
-
-
